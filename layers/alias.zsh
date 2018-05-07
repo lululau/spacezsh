f75520a0c6332f57f4cc66e834366f1172b213ac
Liu Xiang <liuxiang@ktjr.com>
2018-05-07 11:05:06 +0800

    Revise alias layer

--- layers/alias.zsh
+++ layers/alias.zsh
@@ -1,15 +1,43 @@
 #!/usr/bin/env zsh
 
-bindkey -s '\eL' 'ls\n'
-bindkey -s '\el\el' 'l\n'
-bindkey -s '\elr' 'l -tr\n'
-bindkey -s '\el\er' 'la -tr\n'
-bindkey -s '\ela' 'la\n'
-bindkey -s '\es' 'ss\n'
+typeset -A SPACEZSH_ALIAS_MAPPINGS=(
+    # ls
+    'll' 'l\n'
+    'lL' 'ls\n'
+    'l1' 'ls -1\n'
+    'lr' 'l -tr\n'
+    'lR' 'la -tr\n'
+    'la' 'la\n'
+    $'\eL' 'ls\n'
+    $'\el\el' 'l\n'
+    $'\elr' 'l -tr\n'
+    $'\el\er' 'la -tr\n'
+    $'\ela' 'la\n'
 
-bindkey -s '\egs' 'git status\n'
-bindkey -s '\egd' 'git diff\n'
-bindkey -s '\egl' 'git log\n'
-bindkey -s '\egp' 'git log -p\n'
-bindkey -s '\eg-' 'git checkout -\n'
-bindkey -s '\eg\eg' 'magit\n'
+    # ssh-dialog
+    'ss' 'ss\n'
+    $'\es' 'ss\n'
+
+    # git
+    'gs' 'git status\n'
+    'gd' 'git diff\n'
+    'gl' 'git log\n'
+    'gp' 'git log -p\n'
+    'g-' 'git checkout -\n'
+    'gg' 'magit\n'
+    $'\egs' 'git status\n'
+    $'\egd' 'git diff\n'
+    $'\egl' 'git log\n'
+    $'\egp' 'git log -p\n'
+    $'\eg-' 'git checkout -\n'
+    $'\eg\eg' 'magit\n'
+)
+
+
+for k (${(k)SPACEZSH_ALIAS_MAPPINGS}); do
+    if [[ "$k" =~ '^[a-zA-Z0-9/]+$' ]]; then
+        bindkey -s "${SPACEZSH_LEADER}$k" "${SPACEZSH_ALIAS_MAPPINGS[$k]}"
+    else
+        bindkey -s "$k" "${SPACEZSH_ALIAS_MAPPINGS[$k]}"
+    fi
+done
