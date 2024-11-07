53d6e4e30e1aaac534c896cba8f46d0e1cac3de8
Liu Xiang <liuxiang921@gmail.com>
2024-11-07 11:45:55 +0800

    layers/kill: Add shortcuts to kill widget
        Add shortcuts to the kill widget to make it easier to kill processes.
        The shortcuts are as follows:
    
        - enter: select PID
        - ctrl-k: kill
        - alt-k: sudo kill
        - ctrl-x: kill -9
        - alt-x: sudo kill -9
        - ctrl-l: lsof
        - alt-l: sudo lsof
    
        The shortcuts are displayed in the header of the fzf widget.

--- layers/kill.zsh
+++ layers/kill.zsh
@@ -7,17 +7,29 @@ spacezsh.kill.fzfcmd_complete() {
 }
 
 spacezsh.kill.widget() {
+  local fzf_expect='enter,ctrl-k,alt-k,ctrl-x,alt-x,ctrl-l,alt-l'
+  local fzf_header=$'\nShortcuts: enter: select PID,\tctrl-k: kill,\talt-k: sudo kill,\tctrl-x: kill -9,\talt-x: sudo kill -9,\tctrl-l: lsof,\talt-l: sudo lsof'
   local FZF_HEIGHT=$([[ -n "$FZF_TMUX" && -n "$TMUX_PANE" ]] && echo ${FZF_TMUX_HEIGHT:-40%} || echo 100%)
-  fzf="$(spacezsh.kill.fzfcmd_complete)"
-  matches=$(ps -ef | sed 1d | FZF_DEFAULT_OPTS="--height ${FZF_HEIGHT} --min-height 15 --reverse $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m -e | awk '{print $2}' | tr '\n' ' ')
-  if [ -n "$matches" ]; then
-    if [ "$KEYS[-1]" = k ]; then
-        RBUFFER="lsof -Pnp $matches"
-    elif [ -n "$LBUFFER" ]; then
-        LBUFFER="$LBUFFER$matches"
-    else
-        LBUFFER="kill $matches"
-    fi
+  local fzf="$(spacezsh.kill.fzfcmd_complete)"
+  local choice=$(ps -ef | sed 1d | FZF_DEFAULT_OPTS="--expect='$fzf_expect' --header='$fzf_header' --header-first --height ${FZF_HEIGHT} --min-height 15 $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m -e)
+  local choices=("${(f)choice}")
+  local operation=$choices[1]
+  local selections=($choices[2,-1])
+  local pids=$(echo "${(F)choices[2,-1]}" | awk -v ORS=' ' '{print $2}')
+  if [[ "$operation" = 'enter' ]]; then
+    LBUFFER+=$pids
+  elif [[ "$operation" = 'ctrl-k' ]]; then
+    LBUFFER="kill $pids"
+  elif [[ "$operation" = 'alt-k' ]]; then
+    LBUFFER="sudo kill $pids"
+  elif [[ "$operation" = 'ctrl-x' ]]; then
+    LBUFFER="kill -9 $pids"
+  elif [[ "$operation" = 'alt-x' ]]; then
+    LBUFFER="sudo kill -9 $pids"
+  elif [[ "$operation" = 'ctrl-l' ]]; then
+    LBUFFER="lsof -Pnp $pids"
+  elif [[ "$operation" = 'alt-l' ]]; then
+    LBUFFER="sudo lsof -Pnp $pids"
   fi
   zle -K main
   zle redisplay
