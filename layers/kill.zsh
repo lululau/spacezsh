b02aa8aed3ea10f82c72a7155b4a1fb8fc13dcc7
Liu Xiang <liuxiang921@gmail.com>
2025-06-19 18:31:56 +0800

    Add pstree support to kill.zsh for process tree visualization
    - Introduce alt-t shortcut to invoke pstree for selected PIDs
    - Update fzf_expect and fzf_header variables to include new alt-t shortcut
    - Maintain consistent structure with existing shortcuts and functionality

--- layers/kill.zsh
+++ layers/kill.zsh
@@ -7,8 +7,8 @@ spacezsh.kill.fzfcmd_complete() {
 }
 
 spacezsh.kill.widget() {
-  local fzf_expect='enter,ctrl-k,alt-k,ctrl-x,alt-x,ctrl-l,alt-l'
-  local fzf_header=$'\nShortcuts: enter: select PID,\tctrl-k: kill,\talt-k: sudo kill,\tctrl-x: kill -9,\talt-x: sudo kill -9,\tctrl-l: lsof,\talt-l: sudo lsof'
+  local fzf_expect='enter,ctrl-k,alt-k,ctrl-x,alt-x,ctrl-l,alt-l,alt-t'
+  local fzf_header=$'\nShortcuts: enter: select PID,\tctrl-k: kill,\talt-k: sudo kill,\tctrl-x: kill -9,\talt-x: sudo kill -9,\tctrl-l: lsof,\talt-l: sudo lsof\talt-t: pstree'
   local FZF_HEIGHT=$([[ -n "$FZF_TMUX" && -n "$TMUX_PANE" ]] && echo ${FZF_TMUX_HEIGHT:-40%} || echo 100%)
   local fzf="$(spacezsh.kill.fzfcmd_complete)"
   local choice=$(ps -ef | sed 1d | FZF_DEFAULT_OPTS="--expect='$fzf_expect' --header='$fzf_header' --header-first --height ${FZF_HEIGHT} --min-height 15 $FZF_DEFAULT_OPTS --preview 'echo {}' --preview-window down:3:wrap $FZF_COMPLETION_OPTS" ${=fzf} -m -e)
@@ -30,6 +30,8 @@ spacezsh.kill.widget() {
     LBUFFER="lsof -Pnp $pids"
   elif [[ "$operation" = 'alt-l' ]]; then
     LBUFFER="sudo lsof -Pnp $pids"
+  elif [[ "$operation" = 'alt-t' ]]; then
+    LBUFFER="pstree $pids"
   fi
   zle -K main
   zle redisplay
